dc5a117ed77b0d15f98b9b64f038892206729baf
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-02-07 12:54:55 +0200

    Set only owner modifier for fee funtions

--- contracts/FeesHandlerUpgradeable.sol
+++ contracts/FeesHandlerUpgradeable.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.20;
 
 //import openzeppli initializable
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-abstract contract FeesHandlerUpgradeable is Initializable {
+abstract contract FeesHandlerUpgradeable is Initializable, OwnableUpgradeable {
     address public _feesFaucet;
     uint256 public _txfeeRate;
     uint256 public _gaslessBasefee;
@@ -15,23 +16,30 @@ abstract contract FeesHandlerUpgradeable is Initializable {
     event GaslessBasefeeUpdated(uint256 newGaslessBasefee);
     event FeeFaucetUpdated(address newFeeFaucet);
     event FeesPaid(address indexed payer, uint256 fees);
-    event GaslessBasefeePaid(address indexed payer, address indexed paymaster, uint256 basefee);
+    event GaslessBasefeePaid(
+        address indexed payer,
+        address indexed paymaster,
+        uint256 basefee
+    );
 
     error InvalidFeeRate(uint256 maxFeeRate, uint256 newFeeRate);
     error NegativeBasefee();
 
-    function setFeeFaucet(address newFeeFaucet) public virtual {
+    function setFeeFaucet(address newFeeFaucet) public virtual onlyOwner {
         _feesFaucet = newFeeFaucet;
         emit FeeFaucetUpdated(newFeeFaucet);
     }
 
-    function setTxFeeRate(uint256 newTxFeeRate) public virtual {
-        if (newTxFeeRate > FEE_RATIO || newTxFeeRate < 0) revert InvalidFeeRate(FEE_RATIO, newTxFeeRate);
+    function setTxFeeRate(uint256 newTxFeeRate) public virtual onlyOwner {
+        if (newTxFeeRate > FEE_RATIO || newTxFeeRate < 0)
+            revert InvalidFeeRate(FEE_RATIO, newTxFeeRate);
         _txfeeRate = newTxFeeRate;
         emit TxFeeRateUpdated(newTxFeeRate);
     }
 
-    function setGaslessBasefee(uint256 newGaslessBasefee) public virtual {
+    function setGaslessBasefee(
+        uint256 newGaslessBasefee
+    ) public virtual onlyOwner {
         if (newGaslessBasefee < 0) revert NegativeBasefee();
         _gaslessBasefee = newGaslessBasefee;
         emit GaslessBasefeeUpdated(newGaslessBasefee);
@@ -55,5 +63,8 @@ abstract contract FeesHandlerUpgradeable is Initializable {
 
     function _payTxFee(address from, uint256 txAmount) internal virtual;
 
-    function payGaslessBasefee(address payer, address paymaster) external virtual;
+    function payGaslessBasefee(
+        address payer,
+        address paymaster
+    ) external virtual;
 }
